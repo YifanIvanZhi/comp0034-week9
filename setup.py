f48f802a04b56b04208c11762ebf6a6c7bf12f9d
nicholsons <sarah.sanders@ucl.ac.uk>
2023-01-31 13:41:08 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup
 
 setup(
-    name="paralympic_app",
-    packages=["paralympic_app"],
-    include_package_data=True,
-    install_requires=["flask", "flask-wtf", "flask-sqlalchemy"],
-)
-
-# Use the following version for the Iris ML app
-"""
-setup(
-    name="iris_app",
-    packages=["iris_app"],
+    name="comp0034-week9",
+    packages=["paralympic_app", "iris_app"],
     include_package_data=True,
     install_requires=[
         "flask",
+        "flask-wtf",
+        "flask-sqlalchemy",
+        "Flask-WTF",
+        "flask-marshmallow",
+        "marshmallow-sqlalchemy",
         "pandas",
-        "sklearn"
+        "requests",
+        "sklearn",
     ],
 )
-"""
